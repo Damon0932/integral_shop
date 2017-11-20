2ad32c81f4ef3f679c0215e46315f1f298eb4901
Gavin <85642319@qq.com>
2017-11-20 12:03:51 +0800

    fix

---
+++ resources/views/shop/beans/detail.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta content="yes" name="apple-mobile-web-app-capable">
+  <meta content="yes" name="apple-touch-fullscreen">
+  <meta content="telephone=no,email=no" name="format-detection">
+  <title>迈豆详情</title>
+  <link rel="stylesheet" href="css/style.css">
+  <script src="http://g.tbcdn.cn/mtb/lib-flexible/0.3.4/flexible.js"></script>
+</head>
+
+<body>
+  <header class="header">
+    <div class="goback"><a href="javascript:;"></a></div>
+    <div class="title">迈豆详情</div>
+  </header>
+  <div class="exchangeDetail">
+    <p>时间：2018-11-24 12:12</p>
+    <p>M豆变动：+10</p>
+    <p>详情：积分转入M豆，您的“迈德分享”微信平台100积分转入</p>
+  </div>
+</body>
+
+</html>
