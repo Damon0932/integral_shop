7a245fb1d216c3682c845cbeb138dafc1c7c6463
Damon <whliujun@hotmail.com>
2017-11-21 11:15:29 +0800

    fix timezone

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Shanghai',
 
     /*
     |--------------------------------------------------------------------------
@@ -229,7 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'EasyWeChat' => Overtrue\LaravelWechat\Facade::class
+        'EasyWeChat' => Overtrue\LaravelWechat\Facade::classy
     ],
 
 ];
