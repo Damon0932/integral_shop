5f4df9580a66c8c947ab63ea6e493e66841da5c5
Gavin <85642319@qq.com>
2017-11-21 17:23:20 +0800

    fix

--- resources/views/shop/beans/month.blade.php
+++ resources/views/shop/beans/month.blade.php
@@ -22,7 +22,7 @@
     <div class="date">2017年11月</div>
     <p class="info">转入：+800 兑换：-300</p>
     <span class="select_time">
-      <img src="images/time_picker.png" alt="">
+      <img src="images/time_picker.png" id="date-selector" alt="">
     </span>
   </div>
   <div class="integral_list">
@@ -41,7 +41,25 @@
       </a>
     </div>
   </div>
+  <div id="targetContainer"></div>
   <script src="https://cdn.bootcss.com/zepto/1.2.0/zepto.min.js"></script>
+  <script src="js/DataSelector.js"></script>
+   <script>
+      new DateSelector({
+       input: 'date-selector',//点击触发插件的input框的id
+       container: 'targetContainer',//插件插入的容器id
+       type: 0,
+       param: [1, 1, 0, 0, 0],
+       //设置['year','month','day','hour','minute'],1为需要，0为不需要,需要连续的1
+       beginTime: [2011, 1],//如空数组默认设置成1970年1月1日0时0分开始，如需要设置开始时间点，数组的值对应param参数的对应值。
+       endTime: [],//如空数组默认设置成次年12月31日23时59分结束，如需要设置结束时间点，数组的值对应param参数的对应值。
+       recentTime: [],//如不需要设置当前时间，被为空数组，如需要设置的开始的时间点，数组的值对应param参数的对应值。
+       success: function (arr) {
+         console.log(arr);
+         //ajax
+       }//回调
+     });
+   </script>
 </body>
 
 </html>
