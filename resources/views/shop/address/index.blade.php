65b2f4ecfa020572e3ae22b96e87eb94c5385690
iherewy <85642319@qq.com>
2017-11-23 21:20:09 +0800

    fix

--- resources/views/shop/address/index.blade.php
+++ resources/views/shop/address/index.blade.php
@@ -6,8 +6,8 @@
 @section('content')
     <div class="addressList">
         @foreach($addresses as $address)
-            <div class="address" data-id="{{$address->id}}">
-                <div class="select" @if($address->default == '1') selected @endif></div>
+            <div class="address">
+                <div class="select @if($address->default == '1') selected @endif"  data-id="{{$address->id}}"></div>
                 <div class="detail">
                     <p>{{$address->province.$address->city.$address->district.$address->address}}</p>
 
@@ -18,7 +18,23 @@
         @endforeach
     </div>
     <div class="addressBtn">
-        <a href="" class="btn">设为默认</a>
+        <a href="javascript:;" class="btn" id="setAddress">设为默认</a>
         <a href="{{route('address.create')}}" class="btn">添加新地址</a>
     </div>
+    <script>
+    $(function($) {
+        var addressid = '';
+        $('.select'),click(function){
+            if($(this).hasClass('selected')) return false;
+            $('.address .select').removeClass('selected');
+            $(this).addClass('selected');
+            addressid = $(this).attr('data-id');
+        }
+        $('#setAddress').click(function(){
+            if(addressid !== ''){
+                window.location.href='/shop/address/'+addressid+'/set-default';
+            }
+        })
+    });
+    </script>
 @endsection
