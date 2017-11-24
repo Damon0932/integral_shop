659303ce69e82d133594a988e1b7f89bd3652b65
Gavin <85642319@qq.com>
2017-11-24 20:04:30 +0800

    fix

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -76,50 +76,23 @@
     </div>
     <script src="https://cdn.bootcss.com/Swiper/3.4.2/js/swiper.jquery.min.js"></script>
     <script>
-       $(window).scroll(function () {
-			var goodsListHeight = $('.goodContent').height(); 
-			var goodsListTop = $('.goodContent').offset().top;
+		 $('#topNav .swiper-slide').click(function(){
+			  $("#topNav  .active").removeClass('active');
+			  $("#topNav .swiper-slide").eq($(this).index()).addClass('active');
+			  $(".goodContent .goodList").css("display", "none");
+			  $(".goodContent .goodList").eq($(this).index()).css("display", "block");
+		  })
 
-			if ($(window).scrollTop() >= goodsListTop && $(window).scrollTop() <= (goodsListHeight + goodsListTop)) {
-			  $('#topNav').addClass('fixNav');
-			} else {
-			  $('#topNav').removeClass('fixNav');
-			}
-		});
+		  $(window).scroll(function () {
+				var goodsListHeight = $('.goodContent').height(); 
+				var goodsListTop = $('.goodContent').offset().top;
 
-        var mySwiper = new Swiper('#topNav', {
-            freeMode: true,
-            slidesPerView: 'auto',
-            freeModeSticky: true,
-            onTap: function (swiper, e) {
-                console.log(swiper.clickedIndex)
-                slide = swiper.slides[swiper.clickedIndex]
-                slideLeft = slide.offsetLeft
-                slideWidth = slide.clientWidth
-                slideCenter = slideLeft + slideWidth / 2
-                // 被点击slide的中心点
-                mySwiper.setWrapperTransition(300)
-                if (slideCenter < swiperWidth / 2) {
-                    mySwiper.setWrapperTranslate(0)
-                } else if (slideCenter > maxWidth) {
-                    mySwiper.setWrapperTranslate(maxTranslate)
-                } else {
-                    nowTlanslate = slideCenter - swiperWidth / 2
-                    mySwiper.setWrapperTranslate(-nowTlanslate)
-                }
-                $("#topNav  .active").removeClass('active');
-                $("#topNav .swiper-slide").eq(swiper.clickedIndex).addClass('active');
-                $(".goodContent .goodList").css("display", "none");
-                $(".goodContent .goodList").eq(swiper.clickedIndex).css("display", "block");
-            }
-        });
-        swiperWidth = mySwiper.container[0].clientWidth
-        maxTranslate = mySwiper.maxTranslate();
-        maxWidth = -maxTranslate + swiperWidth / 2
-
-        $(".swiper-container").on('touchstart', function (e) {
-            e.preventDefault()
-        })
+				if ($(window).scrollTop() >= goodsListTop && $(window).scrollTop() <= (goodsListHeight + goodsListTop)) {
+				  $('#topNav').addClass('fixNav');
+				} else {
+				  $('#topNav').removeClass('fixNav');
+				}
+			});
 
     </script>
 @endsection
