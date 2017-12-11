bd3a094082d26b4b5bc2acbb6321a21250be1ad1
Damon <whliujun@hotmail.com>
2017-12-11 13:09:32 +0800

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
         ],
-        'shop' => ['wechat.oauth', 'beans']
+        'shop' => ['wechat.oauth:snsapi_userinfo', 'beans']
     ];
 
     /**
